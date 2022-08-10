984c3f6ac7cf7c293dc76485e2eb710de7862b9c
mbahgatTech <mazenbahgat@outlook.com>
2022-08-10 14:03:42 -0400

    UI entry deletion feedback and bug fix

--- app/components/Entry.jsx
+++ app/components/Entry.jsx
@@ -24,7 +24,7 @@ const Entry = ({data, allEntries, onPress, setRerender, onShareDelete}) => {
                 return;
             }
 
-            onShareDelete(data?.field, user?.userInfo?.username);
+            onShareDelete(data?.field, user, setRerender);
             return;
         }
 
